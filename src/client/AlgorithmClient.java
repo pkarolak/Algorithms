dbee1ed85eb7f955e5107c8987908e7e76892195
Arawis <limonka07@gmail.com>
2014-04-03 17:23:00 +0200

    my first commit

--- src/client/AlgorithmClient.java
+++ src/client/AlgorithmClient.java
@@ -4,11 +4,12 @@ import sortable.*;
 public class AlgorithmClient {
 	public static void main(String[] args) {
 		SortableArray myArray = new SortableArray(SortableArray.SIZE,SortableArray.RANGE);
-		//myArray.PrintArray();
+		myArray.PrintArray();
 		myArray.BubbleSort();
 		myArray.InsertionSort();
 		myArray.SelectionSort();
 		myArray.CountSort();
+		myArray.PrintArray();
 	}
 
 }
